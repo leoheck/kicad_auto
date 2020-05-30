538e144b9095b0551bccd1cd42c021d4e20269d3
Leandro <leoheck@gmail.com>
2020-05-30 01:23:56 -0300

    Minor improvements

--- download.sh
+++ download.sh
@@ -1,6 +1,19 @@
-#!/bin/sh
-rm *.deb
-curl -s https://api.github.com/repos/INTI-CMNB/kicad-automation-scripts/releases/latest | grep "browser_download_url.*deb" | cut -d : -f 2,3 | tr -d \" | wget -i -
-curl -s https://api.github.com/repos/INTI-CMNB/InteractiveHtmlBom/releases/latest | grep "browser_download_url.*deb" | cut -d : -f 2,3 | tr -d \" | wget -i -
-curl -s https://api.github.com/repos/INTI-CMNB/kiplot/releases/latest | grep "browser_download_url.*deb" | cut -d : -f 2,3 | tr -d \" | wget -i -
-curl -s https://api.github.com/repos/INTI-CMNB/KiBoM/releases/latest | grep "browser_download_url.*deb" | cut -d : -f 2,3 | tr -d \" | wget -i -
+#!/bin/bash
+
+donwload_release()
+{
+	release_url=$1
+	curl \
+		-s $release_url | \
+		grep "browser_download_url.*deb" | \
+		cut -d : -f 2,3 | \
+		tr -d \" | \
+		wget -i -
+}
+
+rm -f *.deb
+
+donwload_release https://api.github.com/repos/INTI-CMNB/kicad-automation-scripts/releases/latest 
+donwload_release https://api.github.com/repos/INTI-CMNB/InteractiveHtmlBom/releases/latest 
+donwload_release https://api.github.com/repos/INTI-CMNB/kiplot/releases/latest 
+donwload_release https://api.github.com/repos/INTI-CMNB/KiBoM/releases/latest 
