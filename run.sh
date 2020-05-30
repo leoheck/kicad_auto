8866942e8f2dfb9c699b38040b7615c34454d6ac
Leandro <leoheck@gmail.com>
2020-05-30 01:23:23 -0300

    Minor formating changes

--- run.sh
+++ run.sh
@@ -1,8 +1,13 @@
+#!/bin/bash
+
 export USER_ID=$(id -u)
 export GROUP_ID=$(id -g)
+
 WORKDIR=../hard
 SUBDIR=Conjunto
-docker run --rm -it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY \
+
+docker run \
+    --rm -it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY \
     -v $(pwd)/$WORKDIR:/home/$USER/workdir \
     --user $USER_ID:$GROUP_ID \
     --env NO_AT_BRIDGE=1 \
@@ -12,4 +17,4 @@ docker run --rm -it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY \
     --volume="/home/$USER/.cache/kicad:/home/$USER/.cache/kicad:rw" \
     --volume="/etc/passwd:/etc/passwd:ro" \
     --volume="/etc/shadow:/etc/shadow:ro" \
-    setsoft/kicad_auto:latest /bin/bash -c "cd workdir/$SUBDIR; kiplot"
+    leoheck/kicad_auto:latest /bin/bash -c "cd workdir/$SUBDIR; kiplot"
